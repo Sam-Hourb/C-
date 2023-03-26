21a471c419870d88fdfffa8d541e43de82cbefff
Sam-Hourb <121263083+Sam-Hourb@users.noreply.github.com>
2023-03-25 19:02:17 -0700

    Create InsertionOperator.cpp

---
+++ Operator Overloading/InsertionOperator.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+class Rectangle
+{
+  private:
+    int width;
+    int length;
+
+  public:
+    Rectangle(){ }
+
+    Rectangle(int w, int l)
+    {
+       width = w;
+       length = l;
+    }
+
+    friend  ostream & operator<<(ostream & outr, Rectangle & r);
+
+    int getLength()
+    {
+      return length;
+    }
+     int getwidth()
+    {
+      return width;
+    }
+};
+
+    ostream & operator<<(ostream & outr, Rectangle & r)
+    {
+       outr << "Length = " << r.width << "  " << "Width = " << r.length << endl;
+       
+       return outr;
+    }
+
+
+int main ()
+{
+  Rectangle r(2,4);
+  Rectangle r2(4,6);
+  Rectangle r4 = r + r2; 
+  
+  cout << r4 << endl << r << endl << r2 << endl;
+  
+}
