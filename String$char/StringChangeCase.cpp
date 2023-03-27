59de8a65f46a8c1ff48aa1b2e23163c3d0e0cefa
Sam-Hourb <121263083+Sam-Hourb@users.noreply.github.com>
2023-03-26 18:24:18 -0700

    Update StringChangeCase.cpp

--- String$char/StringChangeCase.cpp
+++ String$char/StringChangeCase.cpp
@@ -5,30 +5,24 @@ using namespace std;
 
 void toUpper(string &ar)
 {
-    const char *arr = ar.c_str();
-
-    for(int i = 0; arr[i] != '\0' ; i++)
+    for(int i = 0; ar[i] != '\0' ; i++)
     {
         if(ar[i] >= 97 && ar[i] <= 122)
         {
             ar[i] = ar[i] - 32;
         }
     }
-
 }
 
 void toLower(string &ar)
 {
-    const char *arr = ar.c_str();
-
-    for(int i = 0; arr[i] != '\0' ; i++)
+    for(int i = 0; ar[i] != '\0' ; i++)
     {
         if(ar[i] >= 65 && ar[i] <= 90)
         {
             ar[i] = ar[i] + 32;
         }
     }
-
 }
 
 int main()
