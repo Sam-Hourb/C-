e3f68a4d3c6deaa41e7ce944be3dec5b77541dc9
Sam-Hourb <121263083+Sam-Hourb@users.noreply.github.com>
2023-03-30 11:21:41 -0700

    Update ArrayADTStruct.cpp

--- ArrayADT/ArrayADTStruct.cpp
+++ ArrayADT/ArrayADTStruct.cpp
@@ -8,75 +8,66 @@ struct Array
     int lenght = 0;
 };
 
-//_______________________________
-
-void create(Array &a)
+void create(Array *a)
 {
     cout << " enter the size of arry: ";
-    cin >> a.size;
-    a.A = new int[a.size];
+    cin >> a->size;
+    a->A = new int[a->size];
 }
 
-//_______________________________
 
-void addLast(Array &a, int e)
+void addLast(Array *a, int e)
 {
-    if(a.lenght < a.size)
+    if(a->lenght < a->size)
     {
-        a.A[a.lenght] = e;
-        a.lenght++;
+        a->A[a->lenght] = e;
+        a->lenght++;
     }
 }
 
-//_______________________________
-
-void insert(Array &a, int i, int e)
+void insert(Array *a, int i, int e)
 {
-    if (i < a.size && i <= a.lenght && a.lenght < a.size && i >= 0)
+    if (i < a->size && i <= a->lenght && a->lenght < a->size && i >= 0)
     {
-        if ( i == a.lenght)
+        if ( i == a->lenght)
         {
-            a.A[a.lenght] = e;
-            a.lenght++;
+            a->A[a->lenght] = e;
+            a->lenght++;
         }
         else 
         {
-            for (int c = a.lenght ; c > i; c-- )
+            for (int c = a->lenght ; c > i; c-- )
             {
-                a.A[c] = a.A[c-1];
+                a->A[c] = a->A[c-1];
             }
 
-            a.A[i] = e;
-            a.lenght++;
+            a->A[i] = e;
+            a->lenght++;
         }
         
     }
 }
 
-//_______________________________
-
-void deleteIndex(Array &a, int index)
+void deleteIndex(Array *a, int index)
 {
-    if (index < a.size && index <= a.lenght && a.lenght <= a.size && index >= 0)
+    if (index < a->size && index <= a->lenght && a->lenght <= a->size && index >= 0)
     {
-         if ( index == a.lenght)
+         if ( index == a->lenght)
         {
-            a.A[a.lenght] = 0;
-            a.lenght--;
+            a->A[a->lenght] = 0;
+            a->lenght--;
         }   
          else 
         {
-            for (int c = index ; c < a.lenght; c++ )
+            for (int c = index ; c < a->lenght; c++ )
             {
-                a.A[c] = a.A[c+1];
+                a->A[c] = a->A[c+1];
             }
-            a.A[a.lenght]= 0;
-            a.lenght--;
+            a->A[a->lenght]= 0;
+            a->lenght--;
         }
     }
 }
-
-//_______________________________
 
 void display(Array a)
 {
@@ -86,21 +77,22 @@ void display(Array a)
     }
 }
 
-//_______________________________
-
 int main()
 {
     Array myArray;
-    
-    create(myArray);
+    create(&myArray);
+    // cout << " enter the size of arry: ";
+    // cin >> myArray.size;
+    // myArray.A = new int[myArray.size];
 
-    addLast(myArray, 1);
-    addLast(myArray, 2);
-    addLast(myArray, 3);
-    addLast(myArray, 4);
-    insert(myArray, 0, 6);
-    addLast(myArray, 7);
-    deleteIndex(myArray, 5);
+    addLast(&myArray, 1);
+    addLast(&myArray, 2);
+    addLast(&myArray, 3);
+    addLast(&myArray, 4);
+    insert(&myArray, 4, 6);
+    addLast(&myArray, 7);
+
+    deleteIndex(&myArray, 5);
 
     display(myArray);
 
